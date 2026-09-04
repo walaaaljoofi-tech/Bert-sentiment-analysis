a1d90bf4edf5adc0e260c4ca37dbf8501dab99ea
walaaaljoofi-tech <walaaaljoofi@gmail.com>
2026-09-04 23:50:01 +0300

    Update sentiment.py

--- src/sentiment.py
+++ src/sentiment.py
@@ -1,1 +1,37 @@
+import torch
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+)
 
+
+MODEL_NAME = "nlptown/bert-base-multilingual-uncased-sentiment"
+
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+model = AutoModelForSequenceClassification.from_pretrained(
+    MODEL_NAME
+)
+
+
+def sentiment_score(review: str) -> int:
+    """
+    Predict a sentiment score from 1 to 5.
+    """
+
+    inputs = tokenizer(
+        review,
+        return_tensors="pt",
+        truncation=True,
+        max_length=512
+    )
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    score = int(
+        torch.argmax(outputs.logits, dim=1).item()
+    ) + 1
+
+    return score
